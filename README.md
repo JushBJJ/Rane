1f20de2082af21bc22592cd2195e5686fcf69077
JushBJJ <JushPacis@gmail.com>
2021-04-13 00:54:27 +1000

    Added create database python script

--- README.md
+++ README.md
@@ -1,25 +1,27 @@
 # Rane Chat Client
+
 ![image](https://user-images.githubusercontent.com/36951064/114356224-b4d15100-9bb3-11eb-9dd7-22f5301016a3.png)
 
 Rane is a chat client aimed to solve the issue of _______ _________ chat client websites. Therefore, this web application is to be ______ _______.
 
 ## How to setup
+
 ### Install Requirements
+
   `pip install -r requirements.txt`
 
-### Generate Database
-TODO
+### Generate Database [/tools]
 
-### Run Resource Server (Terminal Tab 1) [repo-root/resources/]
-  `python resource_server.py`
-  
-### Run Website Server (Terminal Tab 2) [repo-root/]
-  `python server.py`
+  `python create_database`
+
+### Run Server [/]
+  `python run.py`
 
 ## Roadmap
+
 - [x] Fully Transition to SQL
   - [x] Register
-- [ ] Create python script to generate database.
+- [x] Create python script to generate database.
 - [ ] Reorganize and comment repository code.
   - [ ] Reorganizing Functions
   - [ ] Function argument types defined
---
+++ tools/create_database.py
@@ -0,0 +1,75 @@
+import sqlite3
+import os
+import shutil
+
+path = "../data2/"
+
+# Create data folder and subdirectories
+os.mkdir(path)
+os.mkdir(path+"/keys/")
+os.mkdir(path+"/rooms/")
+os.mkdir(path+"/server/")
+
+# Create Databases
+databases = {
+    "./room_template.db": ['''
+        CREATE TABLE "Admins" (
+        	"Username"	TEXT UNIQUE,
+        	"ID"	INTEGER UNIQUE
+        );''', '''
+        CREATE TABLE "Blacklisted" (
+        	"Username"	TEXT UNIQUE,
+        	"ID"	INTEGER UNIQUE
+        );''', '''
+        CREATE TABLE "Invites" (
+        	"Code"	TEXT UNIQUE
+        );''', '''
+        CREATE TABLE "Messages" (
+        	"message_id"	INTEGER,
+        	"author_id"	INTEGER,
+        	"author_ip"	TEXT,
+        	"message"	TEXT,
+        	"show"	INTEGER,
+        	PRIMARY KEY("message_id" AUTOINCREMENT)
+        );''', '''
+        CREATE TABLE "Name" (
+        	"Name"	TEXT
+        );''', '''
+        CREATE TABLE "Owners" (
+        	"Username"	TEXT UNIQUE,
+        	"ID"	INTEGER UNIQUE
+        );''', '''
+        CREATE TABLE "Whitelisted" (
+        	"Username"	TEXT,
+        	"ID"	INTEGER
+        );'''],
+    "./rooms.db": ['''
+        CREATE TABLE "Rooms" (
+        	"Name"	TEXT,
+        	"Description"	TEXT,
+        	"ID"	TEXT UNIQUE,
+        	"Public"	INTEGER
+        );
+    '''],
+    "./server_info.db": ['''
+        CREATE TABLE "online" (
+        	"username"	TEXT UNIQUE
+        );
+    ''']
+}
+
+for database in databases.keys():
+    db_path = path+database
+
+    db = sqlite3.connect(db_path)
+
+    cur = db.cursor()
+    for command in databases[database]:
+        cur.execute(command)
+
+    cur.close()
+    db.commit()
+
+
+# Create Genesis Room
+shutil.copyfile(path+"./room_template.db", path+"/rooms/0.db")
